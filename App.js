cd363e8ede3e3fc35f2a601e97d6199f4b1b1708
lissinSz <alissinbk2@gmail.com>
2026-03-03 22:46:40 -0300

    Atividade 1

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 
 import Exemplo01 from './src/exemplos/ex-01';
+import Atividade01 from './src/atividades/ativ-01';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Exemplo01 />
+      <Atividade01 />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/atividades/ativ-01/index.js
@@ -0,0 +1,18 @@
+import { View, Text} from 'react-native';
+
+import styles from './styles';
+
+function Atividade01 () {
+    return(
+        <View style={styles.container} >
+            <Text style={styles.titulo} >Exemplo 1</Text>
+            <Text>
+                Aula de React Native com Expo
+            </Text>
+
+            <Text>Um texto qualquer</Text>
+        </View>
+    );
+}
+
+export default Atividade01;
